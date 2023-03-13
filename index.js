ac28ab336c3f797dbfa0df878c5f0baf75851ae2
catherine <wachukimani@gmail.com>
2023-03-14 01:44:31 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+
+
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400){
+    return 'This one is on me!'
+
+  } else if (ride > 2000 && ride <= 2500){
+    return 'I will gladly take your thirty bucks.'
+
+  } else if (ride > 2500) {
+    return 'No can do.'
+
+  }
+  
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
+ 
 }
 
-function switchOnCharmFromTip(){
+
+
+function switchOnCharmFromTip(tip){
+
+  if (tip === 'generous'){
+
+    return 'Thank you so much.'
+  }
+
+  else if (tip === 'not as generous'){
+
+    return 'Thank you.'
+  }
+
+  else {
+
+    return 'Bye.'
+  }
   // Write your code here!
 }
